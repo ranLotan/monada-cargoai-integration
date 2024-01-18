fb9ffe2acd30836c32ba8ab6bcc4d099540d2299
3LOK <yoavamit@yahoo.com>
2024-01-18 02:16:23 +0200

    New card design

--- src/client/DetailsCard.js
+++ src/client/DetailsCard.js
@@ -1,24 +1,12 @@
 import React from 'react';
 
 // MUI imports
-import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import TableBody from '@mui/material/TableBody';
-import TableHead from '@mui/material/TableHead';
-import TableContainer from '@mui/material/TableContainer';
-import { yellow } from '@mui/material/colors';
+import { grey } from '@mui/material/colors';
 
 // Font Awesome import
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar, faGripDots } from '@fortawesome/pro-solid-svg-icons';
-import { faGlobe } from '@fortawesome/pro-light-svg-icons';
-
-// Monada imports
-import SupplierLogos from '@monada-ai/monada-shipping-companies-logos';
+import { faGripDotsVertical } from '@fortawesome/pro-solid-svg-icons';
 
 // Generic imports
 import _ from 'lodash';
@@ -26,19 +14,127 @@ import { useDrag } from 'react-dnd'
 import CurrencyList from 'currency-list';
 
 function DetailsCard(props) {
-    const { rate, toggleFavorite, isFavorite, emphasize } = props;
+    const { rate, toggleFavorite, isFavorite, emphasize, routeIndex = 0, departIndex = 0 } = props;
 
     const attributes = _.get(rate, 'attributes.okargoOffer');
-    const originPort = _.get(attributes, 'routes[0].originPort', '');
-    const destinationPort = _.get(attributes, 'routes[0].destinationPort', '');
-    const transitDays = _.get(attributes, 'routes[0].bestTransitTime');
+    const originPort = _.get(attributes, `routes[${routeIndex}].originPort`, '');
+    const destinationPort = _.get(attributes, `routes[${routeIndex}].destinationPort`, '');
+    const transitDays = _.get(attributes, `routes[${routeIndex}].bestTransitTime`);
     const dateStart = _.get(attributes, 'chargeSet.dateBegin').replace(/T00:00:00/, '');
     const dateEnd = (_.get(attributes, 'chargeSet.dateEnd') || '').replace(/T00:00:00/, '');
     const expiration = (_.get(attributes, 'chargeSet.quotValidity') || '').replace(/T00:00:00/, '');
     const routes = _.get(attributes, 'routes');
+    const vesselName = _.get(attributes, `routes[${routeIndex}].departs[${departIndex}].vessel.name`, null);
+    const vesselUid = _.get(attributes, `routes[${routeIndex}].departs[${departIndex}].uid`, 'unknown'); 
+    const vesselImo = _.get(attributes, `routes[${routeIndex}].departs[${departIndex}].vessel.imo`, null);
+    const vesselService = _.get(attributes, `routes[${routeIndex}].serviceCode`, null);
+    const ratesPriceType = _.get(attributes, 'chargeSet.ratesPriceType', null); 
+    const moreInfo = _.get(attributes, 'offerInformations', []); 
+
 
     return (
         <Box>
+            {rate.offer.sections.map(section => {
+                return (
+                    <Box key={section.title} sx={{ marginBottom: '20px' }}>
+                        <Box sx={{ fontWeight: 400, fontSize: '13px', textTransform: 'uppercase', marginBottom: '32px' }}>
+                            {section.title}
+                        </Box>
+                        <Box sx={{ display: 'flex', marginTop: '12px', flexWrap: 'wrap' }}>
+                            {section.offers[0].fields.map(field => {
+                                const value = _.values(field.values)[0];
+                                return (
+                                    <OkargoSingleFieldDetails key={field.id} rate={rate} field={field} value={value} emphasize={!!(emphasize || []).find(e => e.rateId === rate.id && e.fieldId === field.id)}/>
+                                )
+                            })}
+                        </Box>
+                    </Box>
+                );
+            })}
+            <Box sx={{ width: '100%', paddingBottom: '20px', borderTop: '1px solid #D9D9D9' }} />
+            <Box sx={{ fontSize: '13px' }}>
+                {new Date(rate.offer.validFrom).getTime() > new Date().getTime() ? `Valid from: ${new Date(rate.offer.validFrom).toLocaleDateString('en-GB')}` : ''}
+                {new Date(rate.offer.validFrom).getTime() <= new Date().getTime() ? `Valid until: ${new Date(rate.offer.validUntil).toLocaleDateString('en-GB')}` : ''}
+                {' · '}
+                Shipping Window: {new Date(dateStart).toLocaleDateString('en-GB')} - {new Date(dateEnd).toLocaleDateString('en-GB')}
+                {vesselName && <>{` · Vessel: ${vesselName}-${vesselUid}${vesselImo ? ' ' + vesselImo : ''}${vesselService ? ' ' + vesselService : ''}`}</>}
+                {ratesPriceType === 'Contract' && <>{` · Rate type: ${ratesPriceType}`}</>}
+                {ratesPriceType && ratesPriceType !== 'Contract' && <>{` · Rate type: ${ratesPriceType}`}</>}
+                {moreInfo.map((o, i) => (<>{i === 0 ? ` · More info: ` : ', '}<i>{o.content}</i></>))}
+            </Box>
+        </Box>
+    )
+}
+
+function OkargoSingleFieldDetails(props) {
+    const { rate, field, value, emphasize } = props;
+
+    const [, drag] = useDrag(() => ({
+        type: 'RateCardSingleRate',
+        item: {
+            rate,
+            field,
+        },
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
+
+    if ((field.type !== 'per-unit') && (field.type !== 'per-unit-type') && (field.type !== 'flat') && (field.type !== 'custom')) return null;
+
+    return (
+        <Box
+            sx={{ 
+                cursor: 'move', 
+                padding: '20px',
+                width: '100%', 
+                minWidth: '100%', 
+                marginBottom: '12px',
+                border: '1px solid #E0E0E0'
+            }}
+        >
+            <Box
+                ref={drag} 
+                sx={{ 
+                    cursor: 'move', 
+                    fontWeight: emphasize ? '800' : '400', 
+                    display: 'flex', 
+                    alignItems: 'center',
+                    width: '100%', 
+                    minWidth: '100%', 
+                }}
+            >
+                <Box sx={{ fontWeight: emphasize ? '800' : '400', fontSize: '18px' }}>
+                    <FontAwesomeIcon icon={faGripDotsVertical} />
+                </Box>
+                <Box sx={{ padding: '0px 22px' }}>
+                |
+                </Box>
+                <Box sx={{ fontSize: '10px', fontWeight: emphasize ? '800' : '400' }}>
+                    {rate.product.type}
+                </Box>
+                <Box sx={{ padding: '0px 22px' }}>
+                |
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Box sx={{ fontSize: '12px', fontWeight: emphasize ? '800' : '400', marginBottom: '6px' }}>
+                        {CurrencyList.get(value.currency).symbol}{value.value.toLocaleString(undefined, {minimumFractionDigits: 2})}
+                        {field.type === 'per-unit' || field.type === 'per-unit-type' ? ' / product' : ''}
+                        {field.type === 'flat' ? ' / shipment' : ''}
+                        {field.type === 'custom' ? ` / ${field.multiplierText}` : ''}
+                    </Box>
+                    <Box sx={{ fontSize: '12px', color: grey[600] }}>
+                        {field.title}
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default DetailsCard;
+
+/*
             <Box style={{ display: 'flex', alignItems: 'center' }}>
                 <SupplierLogos size={SupplierLogos.size.LARGE} organization={rate.supplier.organization} />
                 <Box sx={{ flex: 1, marginLeft: '12px' }}>
@@ -71,24 +167,8 @@ function DetailsCard(props) {
                     Size & Type: <b>{rate.product.type}{rate.product.dangerous ? ' (Dangerous)' : ''}</b>
                 </Box>
             </Box>
-            {rate.offer.sections.map(section => {
-                return (
-                    <Box key={section.title} sx={{ marginTop: '12px' }}>
-                        <Box sx={{ fontWeight: 800, fontSize: '15px'}}>{section.title}</Box>
-                        <Box sx={{ display: 'flex', marginTop: '12px', flexWrap: 'wrap' }}>
-                            {section.offers[0].fields.map(field => {
-                                const value = _.values(field.values)[0];
-                                return (
-                                    <OkargoSingleFieldDetails key={field.id} rate={rate} field={field} value={value} emphasize={!!(emphasize || []).find(e => e.rateId === rate.id && e.fieldId === field.id)}/>
-                                )
-                            })}
-                        </Box>
-                    </Box>
-                );
-            })}
-            <Box sx={{ marginTop: '18px', fontSize: '17px', fontWeight: 800 }}>
-                Shipping Schedule
-            </Box>
+            */
+                            /*
             <TableContainer>
                 <Table>
                     <TableHead>
@@ -125,44 +205,4 @@ function DetailsCard(props) {
                     </TableBody>
                 </Table>
             </TableContainer>
-        </Box>
-    )
-}
-
-function OkargoSingleFieldDetails(props) {
-    const { rate, field, value, emphasize } = props;
-
-    const [, drag] = useDrag(() => ({
-        type: 'RateCardSingleRate',
-        item: {
-            rate,
-            field,
-        },
-        collect: (monitor) => ({
-            isDragging: !!monitor.isDragging()
-        })
-    }))
-
-    if ((field.type !== 'per-unit') && (field.type !== 'per-unit-type') && (field.type !== 'flat') && (field.type !== 'custom')) return null;
-
-    return (
-        <Paper ref={drag} sx={{ cursor: 'move', fontWeight: emphasize ? '800' : '400', display: 'flex', flexDirection: 'column', padding: '6px', textAlign: 'center', margin: '0px 12px 12px 0px', width: '140px', minWidth: '140px', marginBottom: '12px' }}>
-            <Box sx={{ fontSize: '13px', fontWeight: emphasize ? '800' : '400', flex: 1 }}>
-                {field.title}
-            </Box>
-            <Box sx={{ fontSize: '13px', fontWeight: emphasize ? '800' : '400', marginTop: '3px' }}>
-                {CurrencyList.get(value.currency).symbol}{value.value.toLocaleString(undefined, {minimumFractionDigits: 2})}
-            </Box>
-            <Box sx={{ fontSize: '12px', fontWeight: emphasize ? '800' : '400', marginTop: '3px', marginBottom: '12px'}}>
-                {field.type === 'per-unit' || field.type === 'per-unit-type' ? '/ product' : ''}
-                {field.type === 'flat' ? '/ shipment' : ''}
-                {field.type === 'custom' ? `/ ${field.multiplierText}` : ''}
-            </Box>
-            <Box sx={{ fontSize: '12px', fontWeight: emphasize ? '800' : '400', marginTop: '3px', marginBottom: '12px' }}>
-                <FontAwesomeIcon icon={faGripDots} />
-            </Box>
-        </Paper>
-    )
-}
-
-export default DetailsCard;
+            */
